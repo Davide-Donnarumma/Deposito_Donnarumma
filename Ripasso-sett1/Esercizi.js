1e87024d1ef0b58c32c19f0e051d56ca9876e30e
Davide-Donnarumma <259861666+Davide-Donnarumma@users.noreply.github.com>
2026-07-08 12:10:23 +0200

    Create Esercizi.js

---
+++ Ripasso-sett1/Esercizi.js
@@ -0,0 +1,24 @@
+// var x = 10;
+
+// if (true) {
+//     var x = 20;
+// }
+
+// console.log(x);
+
+
+// let x = 10;
+
+// if (true) {
+//     let x = 20;
+// }
+
+// console.log(x);
+
+const user = {
+    name: "Mario"
+};
+
+user.name = "Luigi";
+
+console.log(user.name);
